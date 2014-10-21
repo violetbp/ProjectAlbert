0d7532139dc87f581deba4a014864b169e73284a
mookie1097 <mookbot@gmail.com>
2014-10-21 20:04:31 +0000

    fixed migration

--- db/migrate/20141021161756_create_jobs.rb
+++ db/migrate/20141021161756_create_jobs.rb
@@ -1,12 +1,12 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
-      t.int :id
-      t.string :file_path
-      t.int :problem_id
+        #t.integer :id
+        t.string :file_path
+        t.integer :problem_id
       
       t.timestamps
     end
-  add_index :create_table, :problem_id
+  #add_index :create_table, :problem_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003160600) do
+ActiveRecord::Schema.define(version: 20141021161756) do
+
+  create_table "jobs", force: true do |t|
+    t.string   "file_path"
+    t.integer  "problem_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "problem_completions", force: true do |t|
     t.string   "username"
